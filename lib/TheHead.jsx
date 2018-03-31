c155c4f4d46174e66e4c7c7fbd2aa17ab163d168
okunishinishi <okunishinishi@gmail.com>
2018-03-31 16:33:41 +0900

    Releasing new package

--- lib/TheHead.jsx
+++ lib/TheHead.jsx
@@ -1,11 +1,11 @@
 'use strict'
 
-import React from 'react'
-import PropTypes from 'prop-types'
 import c from 'classnames'
+import { expand } from 'objnest'
+import PropTypes from 'prop-types'
+import React from 'react'
 import { spinalcase } from 'stringcase'
 import { resolve as resolveUrl } from 'url'
-import { expand } from 'objnest'
 
 const addQuery = (url, query) => [url, query].join(/\?/.test(url) ? '&' : '?')
 const viewPortString = (values) => Object.keys(values || {}).map((key) =>
@@ -17,107 +17,6 @@ const stringify = (vars) => vars ? JSON.stringify(vars) : 'null'
  * Head of the-components
  */
 class TheHead extends React.Component {
-  render () {
-    const {props} = this
-    const {
-      id,
-      className,
-      charSet,
-      title,
-      icon,
-      base,
-      baseTarget,
-      viewPort,
-      children,
-      js,
-      css,
-      metaContents,
-      metaProperties,
-      itemProps,
-      color,
-      manifest,
-      fallbackUnless
-    } = props
-
-    const globals = expand(props.globals || {})
-
-    const fallbackScript = this.getFallbackScript(fallbackUnless)
-    return (
-      <head className={c('the-head', className)}
-            {...{id}}
-      >
-
-        {charSet && (<meta className='the-head-charset' charSet={charSet}/>)}
-        {base && <base className='the-head-base' href={base} target={baseTarget}/>}
-        {title && (<title className='the-head-title'>{title}</title>)}
-        {icon && (<link className='the-head-icon' rel='icon' href={this.urlFor(icon)}/>)}
-        {viewPort && (<meta className='the-head-viewport' name='viewport' content={viewPortString(viewPort)}/>)}
-        {
-          metaContents && Object.keys(metaContents).map((name) => (
-            <meta className='the-head-meta-content' key={name} name={name} content={metaContents[name]}/>
-          ))
-        }
-        {
-          metaProperties && Object.keys(metaProperties).map((name) => (
-            <meta className='the-head-meta-property' key={name} name={name} property={metaProperties[name]}/>
-          ))
-        }
-        {
-          itemProps && Object.keys(itemProps).map((name) => (
-            <meta className='the-head-item-prop' key={name} itemProp={name} content={metaContents[name]}/>
-          ))
-        }
-        {color && (<meta className='the-head-theme-color' name='theme-color' content={color}/>)}
-        {manifest && (<link className='the-head-manifest' rel='manifest' href={color}/>)}
-
-        {
-          [].concat(css).filter(Boolean).map((url) => (
-            <link rel='stylesheet'
-                  type='text/css'
-                  key={url}
-                  className='the-head-css'
-                  href={this.urlFor(url)}/>
-          ))
-        }
-        {
-          Object.keys(globals).map((name) => (
-            <script type='text/javascript'
-                    key={name}
-                    className='the-head-globals'
-                    dangerouslySetInnerHTML={
-                      {__html: `window.${name}=${stringify(globals[name])}`}
-                    }>
-            </script>
-          ))
-        }
-        {
-          [].concat(js).filter(Boolean).map((url) => (
-            <script type='text/javascript'
-                    key={url}
-                    defer
-                    className='the-head-js'
-                    src={this.urlFor(url)}>
-            </script>
-          ))
-        }
-        {
-          fallbackScript && (
-            <script dangerouslySetInnerHTML={{
-              __html: fallbackScript
-            }}>
-            </script>
-          )
-        }
-        {children}
-      </head>
-    )
-  }
-
-  getVersionQuery () {
-    const {versionKey, version} = this.props
-    return [versionKey, version].join('=')
-  }
-
   getFallbackScript (fallbackUnless) {
     if (!fallbackUnless) {
       return null
@@ -142,6 +41,107 @@ document.addEventListener('DOMContentLoaded', function(event) {
     `.trim()
   }
 
+  getVersionQuery () {
+    const {version, versionKey} = this.props
+    return [versionKey, version].join('=')
+  }
+
+  render () {
+    const {props} = this
+    const {
+      base,
+      baseTarget,
+      charSet,
+      children,
+      className,
+      color,
+      css,
+      fallbackUnless,
+      icon,
+      id,
+      itemProps,
+      js,
+      manifest,
+      metaContents,
+      metaProperties,
+      title,
+      viewPort,
+    } = props
+
+    const globals = expand(props.globals || {})
+
+    const fallbackScript = this.getFallbackScript(fallbackUnless)
+    return (
+      <head className={c('the-head', className)}
+            {...{id}}
+      >
+
+        {charSet && (<meta charSet={charSet} className='the-head-charset'/>)}
+        {base && <base className='the-head-base' href={base} target={baseTarget}/>}
+        {title && (<title className='the-head-title'>{title}</title>)}
+        {icon && (<link className='the-head-icon' href={this.urlFor(icon)} rel='icon'/>)}
+        {viewPort && (<meta className='the-head-viewport' content={viewPortString(viewPort)} name='viewport'/>)}
+        {
+          metaContents && Object.keys(metaContents).map((name) => (
+            <meta className='the-head-meta-content' content={metaContents[name]} key={name} name={name}/>
+          ))
+        }
+        {
+          metaProperties && Object.keys(metaProperties).map((name) => (
+            <meta className='the-head-meta-property' key={name} name={name} property={metaProperties[name]}/>
+          ))
+        }
+        {
+          itemProps && Object.keys(itemProps).map((name) => (
+            <meta className='the-head-item-prop' content={metaContents[name]} itemProp={name} key={name}/>
+          ))
+        }
+        {color && (<meta className='the-head-theme-color' content={color} name='theme-color'/>)}
+        {manifest && (<link className='the-head-manifest' href={color} rel='manifest'/>)}
+
+        {
+          [].concat(css).filter(Boolean).map((url) => (
+            <link className='the-head-css'
+                  href={this.urlFor(url)}
+                  key={url}
+                  rel='stylesheet'
+                  type='text/css'/>
+          ))
+        }
+        {
+          Object.keys(globals).map((name) => (
+            <script className='the-head-globals'
+                    dangerouslySetInnerHTML={
+                      {__html: `window.${name}=${stringify(globals[name])}`}
+                    }
+                    key={name}
+                    type='text/javascript'>
+            </script>
+          ))
+        }
+        {
+          [].concat(js).filter(Boolean).map((url) => (
+            <script className='the-head-js'
+                    defer
+                    key={url}
+                    src={this.urlFor(url)}
+                    type='text/javascript'>
+            </script>
+          ))
+        }
+        {
+          fallbackScript && (
+            <script dangerouslySetInnerHTML={{
+              __html: fallbackScript,
+            }}>
+            </script>
+          )
+        }
+        {children}
+      </head>
+    )
+  }
+
   urlFor (url) {
     const {cdn} = this.props
     const vQuery = this.getVersionQuery()
@@ -157,62 +157,62 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 TheHead.propTypes = {
   /** CSS class name */
-  className: PropTypes.string,
-  /** DOM Id */
-  id: PropTypes.string,
+  /** Base url */
+  base: PropTypes.string,
+  /** Target of base url. '_blank', '_parent', '_self', '_top' or frame name */
+  baseTarget: PropTypes.string,
+  /** CDN URL */
+  cdn: PropTypes.string,
   /** Char set */
   charSet: PropTypes.string,
-  /** Document title */
-  title: PropTypes.string,
-  /** Favicon url */
-  icon: PropTypes.string,
+  className: PropTypes.string,
+  /** Theme color */
+  color: PropTypes.string,
+  /** Global property for fall back */
+  fallbackUnless: PropTypes.string,
   /** Global variables */
   globals: PropTypes.object,
+  /** Favicon url */
+  icon: PropTypes.string,
+  /** DOM Id */
+  id: PropTypes.string,
+  /** Item props */
+  itemProps: PropTypes.object,
+  /** Path of manifest.json */
+  manifest: PropTypes.string,
+  /** Met contents */
+  metaContents: PropTypes.object,
+  /** Met properties */
+  metaProperties: PropTypes.object,
+  /** Document title */
+  title: PropTypes.string,
   /** Version string */
   version: PropTypes.string,
   /** Key for version query */
   versionKey: PropTypes.string,
   /** View port settings */
   viewPort: PropTypes.object,
-  /** Base url */
-  base: PropTypes.string,
-  /** Target of base url. '_blank', '_parent', '_self', '_top' or frame name */
-  baseTarget: PropTypes.string,
-  /** Met contents */
-  metaContents: PropTypes.object,
-  /** Met properties */
-  metaProperties: PropTypes.object,
-  /** Item props */
-  itemProps: PropTypes.object,
-  /** Theme color */
-  color: PropTypes.string,
-  /** Path of manifest.json */
-  manifest: PropTypes.string,
-  /** CDN URL */
-  cdn: PropTypes.string,
-  /** Global property for fall back */
-  fallbackUnless: PropTypes.string
 }
 
 TheHead.defaultProps = {
-  className: null,
-  id: null,
-  charSet: 'utf-8',
-  title: null,
-  icon: null,
-  globals: {},
-  version: 'unknown',
-  versionKey: 'v',
   base: null,
   baseTarget: undefined,
-  viewPort: {width: 'device-width', initialScale: '1.0'},
+  cdn: null,
+  charSet: 'utf-8',
+  className: null,
+  color: null,
+  fallbackUnless: null,
+  globals: {},
+  icon: null,
+  id: null,
+  itemProps: {},
+  manifest: null,
   metaContents: {},
   metaProperties: {},
-  itemProps: {},
-  color: null,
-  manifest: null,
-  cdn: null,
-  fallbackUnless: null
+  title: null,
+  version: 'unknown',
+  versionKey: 'v',
+  viewPort: {initialScale: '1.0', width: 'device-width'},
 }
 
 TheHead.displayName = 'TheHead'
